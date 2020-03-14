42525835ed232b8be423d0f590493b9cd0dd178e
Simo Khouili <m.simokhouili@gmail.com>
2020-03-14 17:53:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,5 +14,23 @@ class Artist
     end
   end
 
+  def add_song(song)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
+
+def add_song_by_name(name)
+  song = Song.new(name)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
+
+def self.song_count
+  @@song_count
+end
+
+
 
 end
